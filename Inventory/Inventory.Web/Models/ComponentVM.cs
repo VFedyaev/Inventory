d2cd986289983c156abdaf6a9117bdc450544963
VFedyaev <VFedyaev@stat.kg>
2018-12-20 12:44:26 +0600

    change name of field in component model

--- Inventory/Inventory.Web/Models/ComponentVM.cs
+++ Inventory/Inventory.Web/Models/ComponentVM.cs
@@ -12,12 +12,12 @@ namespace Inventory.Web.Models
         [Display(Name = "Тип комплектующего")]
         public Guid ComponentTypeId { get; set; }
 
-        [Display(Name = "Наименование модели")]
+        [Display(Name = "Марка")]
         [StringLength(100, ErrorMessage = "Длина строки не должна превышать 100 символов")]
         [Required(ErrorMessage = "Заполните поле!")]
         public string ModelName { get; set; }
 
-        [Display(Name = "Наименование")]
+        [Display(Name = "Модель")]
         [StringLength(100, ErrorMessage = "Длина строки не должна превышать 100 символов")]
         public string Name { get; set; }
 
