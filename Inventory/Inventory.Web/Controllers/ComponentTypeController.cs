ca5c74a83e3b3f171a236844e1cb1e98d3a1fc84
VFedyaev <VFedyaev@stat.kg>
2019-01-31 17:24:26 +0600

    fix component type pagination

--- Inventory/Inventory.Web/Controllers/ComponentTypeController.cs
+++ Inventory/Inventory.Web/Controllers/ComponentTypeController.cs
@@ -60,7 +60,7 @@ namespace Inventory.Web.Controllers
                 return HttpNotFound();
             }
         }
-
+        
         [Authorize(Roles = "admin, manager")]
         public ActionResult Create()
         {
